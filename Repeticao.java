384e03dc42a8e96c1e0177b6f2e889f908dba85b
Fernando Figueiredo <nandofigueiredo.2010@gmail.com>
2020-09-04 21:15:44 -0300

    Primeiro commit

---
+++ Repeticao.java
@@ -0,0 +1,16 @@
+// Fazendo a tabuada de 10
+
+public class Repeticao {
+
+	public static void main(String[] args) {
+		
+		int valorTabuada = 10;
+		
+		for(int i = 1; i<=valorTabuada ;i++) {
+			int conta = valorTabuada*i;
+			System.out.println(valorTabuada+" x "+i+"= "+ conta);
+			
+		}
+	}
+
+}
