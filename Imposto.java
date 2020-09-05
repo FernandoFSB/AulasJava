209bcbc4edb0a9a6c1f9d1ece11caa4fc4e0ca0b
FernandoFSB <62119805+FernandoFSB@users.noreply.github.com>
2020-09-04 21:53:57 -0300

    Add files via upload

---
+++ Imposto.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulaexemplo;
+
+/**
+ *
+ * @author nando
+ */
+/*
+ * Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no 
+ * mês. Calcule e mostre o total do seu salário no referido mês, 
+ * sabendo-se que são descontados 11% para o Imposto de Renda, 
+ * 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+○ salário bruto.
+○ quanto pagou ao IPRF
+○ quanto pagou ao INSS.
+○ quanto pagou ao sindicato.
+○ o salário líquido.
+○ o valor descontado.
+ */
+
+import java.util.Scanner;
+
+
+public class Imposto {
+
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in); 
+		
+		double qtdHoras, iprf, inss, sindicato, salarioBruto, salarioLiquido, desconto;
+		int horas;
+		
+		System.out.println("Quanto você ganha por hora? ");
+		qtdHoras = leitor.nextDouble();
+		System.out.println("Quantas horas você trabalha na semana? "); 
+		horas = leitor.nextInt();
+		
+		salarioBruto = qtdHoras * horas;
+		iprf = salarioBruto * 0.11;
+		inss = salarioBruto * 0.08;
+		sindicato = salarioBruto * 0.05;
+		salarioLiquido = salarioBruto - iprf - inss - sindicato;
+		desconto = iprf + inss + sindicato;
+		
+		System.out.println("O salario bruto é de R$"+ salarioBruto+", o Imposto de renda é de R$"+ iprf+", o INSS é de R$"+inss+", o sindicato é de R$"+sindicato+", o seu salário líquido é de R$"+salarioLiquido+", e o valor do desconto do em seu salário é de R$"+ desconto);
+		
+
+	}
+
+}
+
