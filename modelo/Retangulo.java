05c287abfda619bb9835b6cc7d06dbb6345a4864
FernandoFSB <62119805+FernandoFSB@users.noreply.github.com>
2020-09-23 19:18:24 -0300

    Add files via upload

--- modelo/Retangulo.java
+++ modelo/Retangulo.java
@@ -3,15 +3,21 @@ package modelo;
 public class Retangulo {
 	private float altura;
 	private float base;
+	private float espessura;
 		
-	public Retangulo (float h, float b) {
+	public Retangulo (float h, float b, float e) {
 		this.altura = h;
 		this.base = b;
-	}
+		this.espessura = e;
+	}	
 	
-	public float calculoAreaRetangulo() {
-		float area;		
-		area = this.altura*this.base;
-		return area;
+	public float calculoAreaRetanguloMaior() {		
+		return this.altura*this.base;
+	}
+	public float calculoAreaRetanguloMenor() {
+		return (this.altura - 4)*(this.base - 4);
+	}
+	public float calculoAreaMoldura() {
+		return calculoAreaRetanguloMaior() - calculoAreaRetanguloMenor();
 	}
 }
