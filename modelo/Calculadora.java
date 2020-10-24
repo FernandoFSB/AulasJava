577949c6b35542d233703c60e76db65eb0d6a285
FernandoFSB <62119805+FernandoFSB@users.noreply.github.com>
2020-10-23 23:52:32 -0300

    Add files via upload

---
+++ modelo/Calculadora.java
@@ -0,0 +1,37 @@
+
+package modelo;
+
+public class Calculadora {
+    private double n1;
+    private double n2;
+
+    public Calculadora(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public Calculadora() {
+    }
+
+    public double getN1() {
+        return n1;
+    }
+
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
+
+    public double getN2() {
+        return n2;
+    }
+
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculadora{" + "n1=" + n1 + ", n2=" + n2 + '}';
+    }
+       
+}
